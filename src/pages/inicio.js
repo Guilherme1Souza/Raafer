76ec018afae021ae28025dee182a85b116fe3d67
Guilherme1Souza <guilhermeeduardo2016@outlook.com>
2025-05-21 19:08:30 -0300

    feat: adicionar seção de processos

--- src/pages/inicio.js
+++ src/pages/inicio.js
@@ -3,6 +3,7 @@ import { Helmet } from 'react-helmet';
 import { HeroComponent } from '../components/Hero';
 import styled from 'styled-components';
 import AnimatedNumber from '../../animated';
+import { Search, MoveRight, Droplet, ShoppingBag, Truck } from 'lucide-react';
 
 const Numbers = styled.div`
   position: absolute;
@@ -43,6 +44,43 @@ const Estatistics = styled.div`
   }
   `;
 
+const Process = styled.div`
+  font-weight: 600;
+  padding-inline: 10.9rem;
+  padding-top: 15rem;
+
+  h2 {
+     font-size: clamp(2.8rem, 2vw + 1.6rem, 3.6rem);
+  }
+`;
+
+const ProcessLoading = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 4rem;
+  gap: 2rem;
+  flex-wrap: wrap;
+  padding: 0 10.9rem;
+`;
+
+const ProcessContent = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: center;
+  padding-top: 4.9rem;
+
+  h3 {
+  text-align: center;
+  font-size: 18px;
+  font-weight: 500;
+  width: 142px;
+  }
+  `;
+
+
 const Home = () => {
   return (
     <>
@@ -77,6 +115,30 @@ const Home = () => {
           </Numbers>
         </Estatistics>
       </div>
+      <Process>
+        <h2>Nosso Processo</h2>
+        <ProcessLoading>
+          <ProcessContent>
+            <Search />
+            <h3>Seleção das melhores frutas</h3>
+          </ProcessContent>
+            <MoveRight />
+          <ProcessContent>
+            <Droplet />
+            <h3>Higienização</h3>
+          </ProcessContent>
+            <MoveRight />
+          <ProcessContent>
+            <ShoppingBag />
+            <h3>Montagem do pedido</h3>
+          </ProcessContent>
+            <MoveRight />
+          <ProcessContent>
+             <Truck />
+            <h3>Saída para entrega</h3>
+          </ProcessContent>
+        </ProcessLoading>
+      </Process>
     </>
   );
 }
