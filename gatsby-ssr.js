0c73115b90806feebfdcf80a3149aea7b48941d9
Guilherme1Souza <guilhermeeduardo2016@outlook.com>
2025-06-18 15:56:20 -0300

    feat: add gatsby-ssr.js for server-side rendering with theme provider

---
+++ gatsby-ssr.js
@@ -0,0 +1,8 @@
+// gatsby-ssr.js
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import theme from "./src/styles/theme";
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={theme}>{element}</ThemeProvider>
+);
