3e22bfe029497e49561439ba2b0718b52c672d21
Guilherme1Souza <guilhermeeduardo2016@outlook.com>
2025-06-18 15:44:49 -0300

    fix: atualizar importações do slick-carousel no gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,14 +1,15 @@
 import React from "react";
-import "./src/styles/reset.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/styles/theme";
 
 export const onClientEntry = () => {
-    if (typeof window !== 'undefined' && window.location.pathname === '/') {
-    }
-  };
+  if (typeof window !== "undefined" && window.location.pathname === "/") {
+    // aqui pode colocar lógica que só roda no client quando for a home
+  }
+};
 
-  export const wrapRootElement = ({ element }) => (
-    <ThemeProvider theme={theme}>{element}</ThemeProvider>
-  );
-  
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={theme}>{element}</ThemeProvider>
+);
