fc7151454262b68a6d65da4583346f12f650da5a
Guilherme1Souza <guilhermeeduardo2016@outlook.com>
2025-06-18 15:08:15 -0300

    fix: corrigir importação do estilo no componente Hero

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,5 +1,5 @@
 import React, {useRef, useState, useEffect} from 'react';
-import { Container, BoxHero, Text, Subtext, Background, Button } from './style.js';
+import { Container, BoxHero, Text, Subtext, Background, Button } from './style';
 import { Nav } from '../nav/index.jsx';
 
 export function HeroComponent({ title, subtitle, buttonText }) {
